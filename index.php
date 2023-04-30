3dcdb1da988061571b59a0441d2a8494796f26bf
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-04-30 20:33:30 +0530

    form responsive, php script responsive

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirm Credentials | SeoulGalli</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./main.css">
+    <link rel="stylesheet" href="./credintials-php.css">
 </head>
 <body>
 <?php
@@ -20,21 +21,28 @@ if(isset($_POST['submit']))
     
 }
 ?>
-<img class="bg-img" src="./Assets/bibmbapbg.jpg" alt="bg-img">
+<!-- <img class="bg-img" src="./Assets/bibmbapbg.jpg" alt="bg-img"> -->
 <div class="main">
     <p>Dear User, Your credentials are saved please confirm the details</p>
     <div class="table-container">
         <table>
             <tr>
                 <th>Date</th>
-                <th>Time</th>
-                <th>People</th>
-            </tr>
-            <tr>
                 <td><?php echo"$date"?></td>
             </tr>
-                
+            <tr>
+                <th>Time</th>
+                <td><?php echo"$time"?></td>
+            </tr>
+            <tr>
+                <th>People</th>
+                <td><?php echo"$people"?></td>
+            </tr>
         </table>
+        <br>
+        <p>please verify your phone no. and email below -</p>
+        <p>+91 <?php echo "$phone"?></p>
+        <p><?php echo "$email"?></p>
     </div>
 </div>
 </body>
